bae4d09ebfa804b7127b31096e1e52dbf877f930
nourrhassann <95025872+nourrhassann@users.noreply.github.com>
2022-03-21 20:52:04 +0100

    Update main.js

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,4 +1,11 @@
 //START OF JAVASCRIPT//
-
-
-//ZOOM-IN & ZOOM-OUT BUTTON//
+//Zoom-in & Zoom-out buttons//
+var fontSize = 1;
+function zoomIn() {
+	fontSize += 0.3;
+	document.body.style.fontSize = fontSize + "em";
+}
+function zoomOut() {
+	fontSize -= 0.3;
+	document.body.style.fontSize = fontSize + "em";
+}
