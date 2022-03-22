cb8737f65d0acb5e9e14e256cd485f9270c7d6b7
nourrhassann <95025872+nourrhassann@users.noreply.github.com>
2022-03-22 09:51:24 +0100

    Update main.js

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,5 +1,4 @@
 //START OF JAVASCRIPT//
-//Zoom-in & Zoom-out buttons//
 var fontSize = 1;
 function zoomIn() {
 	fontSize += 0.3;
@@ -9,3 +8,11 @@ function zoomOut() {
 	fontSize -= 0.3;
 	document.body.style.fontSize = fontSize + "em";
 }
+//MAP//
+function myMap() {
+	var mapProp= {
+	  center:new google.maps.LatLng(51.508742,-0.120850),
+	  zoom:5,
+	};
+	var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+	}
